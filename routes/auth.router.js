8b55bda06293fa841e70e3686223169d665ea363
IvanZaborskih <ivan.31.03@mail.ru>
2022-08-19 22:42:24 +0300

    Add validation for user sign in

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -4,7 +4,12 @@ const authController = require('../controllers/auth.controller');
 const { check } = require('express-validator');
 
 router.post('/signin', [
-	check('password', 'Password must have more 8 symbols').isLength({ min: 8 })
+	check('password')
+		.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).withMessage('The password must contain at least eight characters, at least one uppercase letter, one lowercase letter and one number'),
+	check('email')
+		.isEmail().withMessage('Not correct email address'),
+	check('nickname')
+		.isLength({ min: 4 }).withMessage('Nickname must be longer than 4 characters')
 ], authController.signIn);
 
 module.exports = router;
