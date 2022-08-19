c3738a6c435985d79e020069424676afa1621c56
IvanZaborskih <ivan.31.03@mail.ru>
2022-08-19 13:27:38 +0300

    Before first migration

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { sequelize } = require('./models/index');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(PORT, () => {
-	console.log(`SERVER STARTED ON PORT ${PORT}`);
-});
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+
+sequelize.authenticate()
+	.then(() => {
+		console.log('Database connected');
+		app.listen(PORT, () => {
+			console.log(`SERVER STARTED ON PORT ${PORT}`);
+		});
+	})
+	.catch(error => console.log(error));
